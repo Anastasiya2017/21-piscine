dd57d162a73da517ff8f3ace2f6179e65cce1c0e
Tyree Lubowitz <tlubowit@e4r8p6.21-school.ru>
2018-09-05 18:15:29 +0300

    +include in main.c

--- day10/ex01/head_file.h
+++ day10/ex01/head_file.h
@@ -6,12 +6,14 @@
 /*   By: tlubowit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/04 22:35:38 by tlubowit          #+#    #+#             */
-/*   Updated: 2018/09/05 16:24:36 by tlubowit         ###   ########.fr       */
+/*   Updated: 2018/09/05 16:40:33 by tlubowit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEAD_FILE_H
 #define HEAD_FILE_H
+
+#include <stdio.h>
 
 void	ft_putchar(char c);
 void	ft_putnbr(int nb);
--- day10/ex01/main.c
+++ day10/ex01/main.c
@@ -6,13 +6,15 @@
 /*   By: tlubowit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/05 14:44:34 by tlubowit          #+#    #+#             */
-/*   Updated: 2018/09/05 16:29:19 by tlubowit         ###   ########.fr       */
+/*   Updated: 2018/09/05 18:10:08 by tlubowit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "head_file.h"
 
 int	main()
 {
 	 int tab[] = {1, 2, 3, 4, 5, 6, 7};
-	ft_foreach(tab, 1337, &ft_putnbr);
+	ft_foreach(tab, 6, &ft_putnbr);
 	return (0);
 }
