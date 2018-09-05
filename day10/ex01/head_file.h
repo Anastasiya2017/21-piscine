80489443583bd965933e24a9bc20594af7f03848
Tyree Lubowitz <tlubowit@e4r8p6.21-school.ru>
2018-09-05 16:34:04 +0300

    ex01

--- day10/ex01/head_file.h
+++ day10/ex01/head_file.h
@@ -6,7 +6,7 @@
 /*   By: tlubowit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/04 22:35:38 by tlubowit          #+#    #+#             */
-/*   Updated: 2018/09/05 15:54:47 by tlubowit         ###   ########.fr       */
+/*   Updated: 2018/09/05 16:24:36 by tlubowit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 void	ft_putchar(char c);
 void	ft_putnbr(int nb);
+void	ft_foreach(int *tab, int length, void (*f)(int));
 
 #endif
